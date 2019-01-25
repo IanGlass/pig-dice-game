36915c7974515e33f76f7633493a6e7a796b7876
IanGlass <37228869+IanGlass@users.noreply.github.com>
2019-01-25 21:13:01 +1300

    Add an extra die, winning score input and alert dialogue for no winning score

--- Source/app.js
+++ Source/app.js
@@ -2,14 +2,17 @@
 GAME RULES:
 
 - The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- In each turn, a player rolls the dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
+
+2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
 */
 
-var scores, roundScore, activePlayer, dice, gamePlaying;
+
+var scores, roundScore, activePlayer, dice1, dice2, gamePlaying;
 newGame();
 
 function newGame() {
@@ -19,7 +22,8 @@ function newGame() {
     activePlayer = 0;
     gamePlaying = true;
     // Remove dice
-    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice1').style.display = 'none';
+    document.querySelector('.dice2').style.display = 'none';
 
     // Reset player scores
     document.querySelector('#current-0').textContent = 0;
@@ -42,17 +46,20 @@ document.querySelector('.btn-new').addEventListener('click', newGame);
 document.querySelector('.btn-roll').addEventListener('click', function () {
     if (gamePlaying) {
         // Randomly generate number between 1 - 6
-        dice = Math.floor(Math.random() * 6 , 1) + 1;
+        dice1 = Math.floor(Math.random() * 6 , 1) + 1;
+        dice2 = Math.floor(Math.random() * 6 , 1) + 1;
 
         // Show dice if first call since newGame()
-        document.querySelector('.dice').style.display = 'block';
-        if (dice === 1) {
+        document.querySelector('.dice1').style.display = 'block';
+        document.querySelector('.dice2').style.display = 'block';
+        if ((dice1 === 1) || (dice2 === 1)) {
             nextPlayer();
         } else {
             // Update the player round score
-            roundScore += dice;
+            roundScore += dice1 + dice2;
             document.querySelector("#current-" + activePlayer).textContent = roundScore;
-            document.querySelector('.dice').src = 'dice-' + dice + '.png';
+            document.querySelector('.dice1').src = 'dice-' + dice1 + '.png';
+            document.querySelector('.dice2').src = 'dice-' + dice2 + '.png';
         }
     }
 });
@@ -69,25 +76,34 @@ function nextPlayer() {
     // Add 'active' class to other player
     document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
     // Hide dice
-    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice1').style.display = 'none';
+    document.querySelector('.dice2').style.display = 'none';
 }
 
 document.querySelector('.btn-hold').addEventListener('click', function () {
     if (gamePlaying) {
-        // Update scores
-        scores[activePlayer] += roundScore;
-        document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
+        var winningScore = document.querySelector('.winning-score').value;
 
-        // Winner!
-        if (scores[activePlayer] >= 100) {
-            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-            document.querySelector('.dice').style.display = 'none';
-            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-            // de-activate roll dice and hold buttons
-            gamePlaying = false;
+        // Make sure winning score is set
+        if (winningScore) {
+            // Update scores
+            scores[activePlayer] += roundScore;
+            document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
+
+            // Winner!
+            if (scores[activePlayer] >= winningScore) {
+                document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+                document.querySelector('.dice1').style.display = 'none';
+                document.querySelector('.dice2').style.display = 'none';
+                document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+                document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+                // de-activate roll dice and hold buttons
+                gamePlaying = false;
+            } else {
+                nextPlayer();
+            }
         } else {
-            nextPlayer();
+            alert("Please enter the winning score");
         }
     }
 });
